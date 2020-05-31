7a833e51656d18f698dddfc707f664a7510a5d4d
ramiAbdelMajid <rami.abdel.majid@outlook.com>
2020-05-31 16:24:09 +0300

    Xcode StoryBoards and Interface Builder 18:  walkthrought

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Guruvise on 5/30/20.
+//  Copyright © 2020 RamiSSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
