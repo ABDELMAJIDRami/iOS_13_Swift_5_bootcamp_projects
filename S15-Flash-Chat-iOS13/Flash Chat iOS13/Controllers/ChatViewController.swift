6cb9d1564ecc9eff7dd392cb6eab773986e060a6
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-05 15:18:04 +0100

    S15-202-How to use a UITableView and Create a Message Model

--- S15-Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ S15-Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -11,11 +11,23 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!  // connected to our storyboard
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    /* The way we populate data in TableView is different from what we used to data with label where we access a label property and assign it.
+       We need To use Delegate (View extensions)
+     */
+    
+    var messages: [Message] = [
+        Message(sender: "a@2.com", body: "Hey!"),
+        Message(sender: "b@2.com", body: "Hello!"),
+        Message(sender: "c@2.com", body: "What's up?"),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self // delegate also
         title = K.appName   // Navigation bar title
         navigationItem.hidesBackButton = true
     }
@@ -31,5 +43,28 @@ class ChatViewController: UIViewController {
             print ("Error signing out: %@", signOutError)
         }
     }
+}
+
+// when our TableView loads up. It is gonna make a request for data. The method that TableView will
+// call are implemented in the delegate below
+extension ChatViewController: UITableViewDataSource {   // Protocol responsible for populating the TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count  // number of cells/rows in our table view
+    }
     
+    /* index path is the current cell that the tableView is requesting some data for. So this method is going to get called for as many rows as you have in your tableView. So currently, our message.count is three. So this is going to return three which means that this method is gonna be called three times and each time it's asking for a cell for a particular row.*/
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // indexPatch (like index) is a position/index of an item
+        // Return UITableViewCell ->  reference our Prototype cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+extension ChatViewController: UITableViewDelegate { // Protocol that handle user interaction with the TableView
+    // exp: when a user click on a Cell, the below method is trigerred
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)    // print cell index the user tapped on
+    }
 }
