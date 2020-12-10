39822d44f0ec15d229548716113d54f9b98edb1c
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-10 15:32:41 +0100

    S17-214- H4X0R News Part 1 - Understanding the List and Identifiable Protocol

---
+++ S17-214-H4X0R News/S17-214-H4X0R News/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  S17-214-H4X0R News
+//
+//  Created by user183479 on 12/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {    // navigation stack - header
+//            List {
+//                Text("Hello, world!")
+//                Text("Hello, world!")
+//            }
+            List(posts) { post in
+                Text(post.title)
+            }
+            // currently we can't change navigation bar background color
+            .navigationBarTitle("H4X0R News")
+        }
+        // .navigationBarTitle("Test") for a reason we are putting this prop inside NaigationView for it to work
+        
+    }
+}
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ S17-214-H4X0R News/S17-214-H4X0R News/S17_214_H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  S17_214_H4X0R_NewsApp.swift
+//  S17-214-H4X0R News
+//
+//  Created by user183479 on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct S17_214_H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
