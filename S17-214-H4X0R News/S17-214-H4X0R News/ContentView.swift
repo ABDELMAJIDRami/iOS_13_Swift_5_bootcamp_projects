3b62edc1cf4a2945e4dd211a2fbe2ee6c544aa2d
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-11 19:32:27 +0100

    S17-216-H4X0R News Part - The Observer Design Pattern

--- S17-214-H4X0R News/S17-214-H4X0R News/ContentView.swift
+++ S17-214-H4X0R News/S17-214-H4X0R News/ContentView.swift
@@ -8,28 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {    // navigation stack - header
 //            List {
 //                Text("Hello, world!")
 //                Text("Hello, world!")
 //            }
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points)) // we can do string interpolation also
+                    Text(post.title)
+                }
             }
             // currently we can't change navigation bar background color
             .navigationBarTitle("H4X0R News")
         }
         // .navigationBarTitle("Test") for a reason we are putting this prop inside NaigationView for it to work
-        
+        .onAppear(perform: {
+            self.networkManager.fetchData()
+        })
     }
 }
 
-let posts: [Post] = [
+//let posts: [Post] = [
 //    Post(id: "1", title: "Hello"),
 //    Post(id: "2", title: "Bonjour"),
 //    Post(id: "3", title: "Hola")
-]
+//]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
