51e83dcf151d9a0e714590a479b32799c5642483
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-20 08:26:00 +0100

    S19-268

--- S19-Todoey-Realm/Todoey/AppDelegate.swift
+++ S19-Todoey-Realm/Todoey/AppDelegate.swift
@@ -27,8 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // So let's go ahead and add our new item, i.e., create in CRUD, and we're going to commit the current state of our Realm. So just as we did with Core Data, we basically created a new piece of data, then we used the context to commit the current state to our persistent container, or in this case, it's our Realm database
         
-        do {// we kept the following line for initilisation but i beleive we can delete it also cz we are initializing it inside CategoryViewController
-            let realm = try Realm() // A Realm instance (also referred to as “a Realm”) represents a Realm database. like persistance container
+        do {// we kept the following line for initilisation but i beleive we can delete it also cz we are initializing it inside CategoryViewController. She said she kep the initialosation to catch any error when we are initializing a new error
+            _ = try Realm() // A Realm instance (also referred to as “a Realm”) represents a Realm database. like persistance container
             // try realm.write { //If the block throws an error, the transaction will be canceled and any changes made before the error will be rolled back.
                 // realm.add(data)
             // }
@@ -66,50 +66,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // can be user triggered or system triggered to reclaime and rellocate resources
         // Saves changes in the applications's managed object context before the application terminates.
-        self.saveContext()
         print("applicationWillTerminate")
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /**
-         The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /**
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
     }
 }
 
