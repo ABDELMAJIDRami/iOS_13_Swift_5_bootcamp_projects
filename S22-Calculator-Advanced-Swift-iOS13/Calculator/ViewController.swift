d43c46f01240cf649a1cefc3165a98f233757455
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-21 12:58:56 +0100

    S22-303-Accumulating Numbers in the Calculator

---
+++ S22-Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isFinishedTypingNumber = true   // that should be displayed on screen
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            // inspect .currentTitle. It is a getter(read-only) computed property. It can also be nil bcz it is optional and we don't want nil values in our calculator
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+
+}
+
