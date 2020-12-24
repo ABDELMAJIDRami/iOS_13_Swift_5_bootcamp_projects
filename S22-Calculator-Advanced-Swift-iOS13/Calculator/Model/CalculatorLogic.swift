54cd5caa34367973a3b4355630a6c3fef9db85f4
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-24 14:39:52 +0100

    S22-314-Guard Let vs. If Let When to Use Which?

--- S22-Calculator-Advanced-Swift-iOS13/Calculator/Model/CalculatorLogic.swift
+++ S22-Calculator-Advanced-Swift-iOS13/Calculator/Model/CalculatorLogic.swift
@@ -11,26 +11,47 @@ import Foundation
 struct CalculatorLogic {
     private var number: Double?
     
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
-            if symbol == "+/-" {
+            switch symbol {
+            case "+/-":
                 return n * -1
-            } else if symbol == "AC" {
+            case "AC":
                 return 0
-            } else if symbol == "%" {
+            case "%":
                 return n * 0.01
-            } else if symbol == "+" {
-                
-            } else if symbol == "=" {
-                
+            case "=":
+                return performTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
-            
         }
-        
+        return nil
+    }
+    
+    func performTwoNumCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            
+            case "-":
+                return n1 - n2
+            case "×":   // copy it from storyboard. this is a symbol not letter x
+            return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:    // throw an error cz this case should not happen
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
         return nil
     }
 }
