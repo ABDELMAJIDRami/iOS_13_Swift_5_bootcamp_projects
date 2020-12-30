55fe2ac1d177b028ac9572d32ec1f7daf08849d6
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-30 09:02:56 +0200

    S24-338-339-Making HTTP GET requests to Wikipedia using Alamofire and Rest

--- S24-WhatFlower/S24-WhatFlower/ViewController.swift
+++ S24-WhatFlower/S24-WhatFlower/ViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 import CoreML
 import Vision
 import Alamofire
-import SwityJson
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+    
     let imagePicker = UIImagePickerController()
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
@@ -24,7 +26,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .camera
     }
-
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // let userPickerImage = info[.originalImage] as? UIImage   // we can pic the original non-edited image
         
@@ -44,8 +46,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let request = VNCoreMLRequest(model: model) { (request, error) in
             let classification = request.results?.first as? VNClassificationObservation
+            // in case classification is nil -> didn't detect the flower -> we can show any message/alert/... to the user, So i won't do guard let as Angela did.
             
             self.navigationItem.title = classification?.identifier.capitalized
+            
+            self.requestInfo(flowerName: classification!.identifier)    // will throw if classification is nil
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
@@ -56,7 +61,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(error)
         }
     }
-        
+    
+    func requestInfo(flowerName: String) {
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+        ]
+        // .responseJSON accpets a closure
+        AF.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+            print(response)
+        }
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
