54514000c78ee800846c79d7ef05b96f307d7b0b
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2020-12-31 15:34:10 +0100

    S27-366-How to Create a 3D Object in Augmented Reality

--- S27-ARDicee/S27-ARDicee/ViewController.swift
+++ S27-ARDicee/S27-ARDicee/ViewController.swift
@@ -22,11 +22,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // inspect each variable/object to understand it. All objects are preceded with SCN cz they are imported from ScenceKit.
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.blue    // .contents: Animatable.
+        
+        cube.materials = [material]     // array of materials cz an object can have multiple materials: u can change the diffuse, the shininess, the metallicness, the texture of it...
+        
+        let node = SCNNode()    // point in 3D space
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)   // animatable. SeeGoodNotes.
+        
+        node.geometry = cube    // assin that node an object/geometry to display
+        
+        sceneView.scene.rootNode.addChildNode(node) // check GoodNotes.
+        
+        sceneView.autoenablesDefaultLighting = true
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        // sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
