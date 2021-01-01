2d5b5cd51598afa9da293a0598a549ad4d87cc93
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2021-01-01 16:15:42 +0200

    S27-369-How to Detect Horizontal Planes in the Real World

--- S27-ARDicee/S27-ARDicee/ViewController.swift
+++ S27-ARDicee/S27-ARDicee/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // inspect vars to understand - there is many other options to append to this array
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -46,12 +49,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.autoenablesDefaultLighting = true
         
          // Create a new scene
-         let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+            // let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {    // Returns the first node in the node’s child node subtree with the specified name. recursively: true to search the entire child node subtree, or false to search only the node’s immediate children.
-        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-        sceneView.scene.rootNode.addChildNode(diceNode)
-        }
+        // if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {    // Returns the first node in the node’s child node subtree with the specified name. recursively: true to search the entire child node subtree, or false to search only the node’s immediate children.
+        // diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+        // sceneView.scene.rootNode.addChildNode(diceNode)
+        //}
         
         // Set the scene to the view
          // sceneView.scene = scene
@@ -63,11 +66,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if ARWorldTrackingConfiguration.isSupported {
             // Create a session configuration
             let configuration = ARWorldTrackingConfiguration()
+            
+            configuration.planeDetection = .horizontal
 
             // Run the view's session
             sceneView.session.run(configuration)
         } else {
             // we can display a message to a user regarding the AR experience on his device
+        }
+    }
+    
+    // ARSCNViewDelegate method: And as it says, it tells the delegate, which is this current view controller, that a SceneKit node corresponding to a new AR anchor(exp: a horizontal anchor(surface)) has been added to the scene. It means that it's detected a horizontal surface and it's given that detected surface a width and a height which is an AR anchor so that we'll be able to use it to place things or to visualize it.
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("plane detecggggted")
+        if anchor is ARPlaneAnchor {
+            // print("plane detected")
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))  // x * z NOT x * y -- inspect "extent": the last 3 lines says that y-component in always 0 cz it is a flat horizontal plan/anchor
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)  // see GoodNotes
+            
+            // grid.png is the asset that Apple provided in their example for us to be able to visualize these plans.
+            // .png files type have transparencies and we can see the materials underneeath!! so we can see through them.
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            plane.materials = [gridMaterial]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+        } else {
+            return
         }
     }
     
