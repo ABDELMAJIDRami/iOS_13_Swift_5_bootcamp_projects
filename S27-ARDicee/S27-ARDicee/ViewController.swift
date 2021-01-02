add684589f7bd62818956490dd646a9a0a05f615
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2021-01-02 11:06:44 +0100

    S27-371-How to Place Our Dice in 3D Using Touch

--- S27-ARDicee/S27-ARDicee/ViewController.swift
+++ S27-ARDicee/S27-ARDicee/ViewController.swift
@@ -84,10 +84,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
             
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+               
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {    // Returns the first node in the node’s child node subtree with the specified name. recursively: true to search the entire child node subtree, or false to search only the node’s immediate children.
+                    diceNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x,
+                                                   y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                                                   z: hitResult.worldTransform.columns.3.z
+                )
+                sceneView.scene.rootNode.addChildNode(diceNode)
+               }
             }
         }
     }
