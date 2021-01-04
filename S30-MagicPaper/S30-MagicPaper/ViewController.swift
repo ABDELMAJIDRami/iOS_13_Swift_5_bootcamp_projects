097a973dc30bbedefa2461439c788c941a1cd018
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2021-01-04 21:27:44 +0100

    S30-395-Video playback in AR

--- S30-MagicPaper/S30-MagicPaper/ViewController.swift
+++ S30-MagicPaper/S30-MagicPaper/ViewController.swift
@@ -50,8 +50,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
         if let imageAnchor = anchor as? ARImageAnchor {
+            let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4") // when we were using images we were ignoring the extension. But when working with SKVideoNode, make sure to write the fullname including extension
+            videoNode.play()
+            // this videoNode is a SpriteKit videoNode -> we need to add it into a sceneKit element, so that we can place the sceneKit element into our SceneView session.
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))   // the root node for all Sprite Kit objects displayed in a view. Allow us to display Sprote Kit objects. Now, this width and height doesn't have to be precise. It's just a guess of the width and height in pixels of your video. So, say, if you have a 720p video, then it would be 720 in height by 1080 by width. So in our case, our video is actually a lot smaller than that. Now, because we're using a 360p video in resolution, that means that it has a width of 480 by 360. So that's what we'll put as the size for our SKScene.
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            videoScene.xScale = -1.0  // scaling it negatively will reverse the image to make it flip in the vertical Y axis
+            videoScene.addChild(videoNode)  // ready for display in 2D. assign it as a material that will cover our plan which is a 2D object
+            
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            plane.firstMaterial?.diffuse.contents = videoScene
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -.pi / 2
             node.addChildNode(planeNode)
