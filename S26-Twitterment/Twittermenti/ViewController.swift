53811c7d753a5700faabcfa0ec7932f6b140563d
Rami Abdel Majid <44524715+ABDELMAJIDRami@users.noreply.github.com>
2021-01-07 10:36:22 +0100

    S26-357-Performing Sentiment Classification

--- S26-Twitterment/Twittermenti/ViewController.swift
+++ S26-Twitterment/Twittermenti/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
+    
+    let sentimentClassifier = try! TweetSentimentClassifer(configuration: MLModelConfiguration()) // If you know that your function call will not throw an exception, you can call the throwing function with try! to disable error propagation. Note that this will throw a runtime exception if an error is actually thrown.
+    
     // check docs (github readme)
     let swifter = Swifter(consumerKey: "", consumerSecret: "")  // initialize Swifter instance and authenticate ourselves with the Twitter servers using our API key and API Secret
     
@@ -19,6 +23,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terible company!") // we forced unwarp it bcz we are testing it we don't care so m uchabout catching error
+        
+        print(prediction.label)
         
         // our sentiment analyser was trained on English tweets, so we can only	interpret the sentiment in English
         // argument 'lang' miust preced argument 'count' cz the order in the method follows the official api which will be transaleted to a search query
